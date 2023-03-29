9a55643e4c2b91e4d3a47118af62063cf3bf4fad
Marius Warlo <mariusiw@uio.no>
2023-03-29 13:14:25 +0200

    Fjerna unødvendige krøllparenteser

--- app/src/main/java/com/example/extremesport/model/SunriseData.kt
+++ app/src/main/java/com/example/extremesport/model/SunriseData.kt
@@ -7,19 +7,16 @@ data class SunriseData(
     val geometry: Geometry,
     val `when`: When,
     val properties: Properties
-    ) {
-}
+    )
 
 data class Geometry(
     val type: String,
     val coordinates: Array<Double>
-){
-}
+)
 
 data class When(
     val interval: Array<String>
-) {
-}
+)
 
 data class Properties(
     val body: String,
@@ -27,31 +24,26 @@ data class Properties(
     val sunset: Sunset,
     val solarnoon: Solarnoon,
     val solarmidnight: Solarmidnight
-){
-}
+)
 
 data class Sunrise(
     val time: String,
     val azimuth: Double
-){
-}
+)
 
 data class Sunset(
     val time: String,
     val azimuth: Double
-) {
-}
+)
 
 data class Solarnoon(
     val time: String,
     val disc_centre_elevation: Double,
     val visible: Boolean
-) {
-}
+)
 
 data class Solarmidnight(
     val time: String,
     val disc_centre_elevation: Double,
     val visible: Boolean
-) {
-}
+)
