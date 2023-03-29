c8dc3f92480bc40769eae72aca67e211f9d20e46
Marius Warlo <mariusiw@uio.no>
2023-03-29 13:21:24 +0200

    Ø

--- app/src/main/java/com/example/extremesport/MainActivity.kt
+++ app/src/main/java/com/example/extremesport/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.extremesport.data.DataSource
 import com.example.extremesport.ui.theme.ExtremeSportTheme
 
 class MainActivity : ComponentActivity() {
