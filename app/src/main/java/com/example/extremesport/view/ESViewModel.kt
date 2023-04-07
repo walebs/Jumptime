8b726297fedeab2a4f6ff0a6145a2af540ffbadc
oleelom <oleelom@uio.no>
2023-04-07 21:36:03 +0200

    Fixed viewmodel update()

--- app/src/main/java/com/example/extremesport/view/ESViewModel.kt
+++ app/src/main/java/com/example/extremesport/view/ESViewModel.kt
@@ -2,6 +2,7 @@ package com.example.extremesport.view
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.extremesport.data.DataSource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -16,18 +17,25 @@ class ESViewModel: ViewModel() {
     val esState: StateFlow<ESUiState> = _esState.asStateFlow()
 
     init {
-        update()
+        val latitude = 59.933333
+        val longitude = 10.716667
+        val altitude = 1
+        val radius = 0
+        val date = "2022-12-18"
+        val offset = "+01:00"
+
+        update(latitude, longitude, altitude, radius, date, offset)
     }
 
-    fun update(latitude: Double, longitude: Double, date: String, offset: String) {
+    fun update(latitude: Double, longitude: Double, altitude: Int, radius: Int, date: String, offset: String) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 // For the appropriate apis, add floats for location.
-                val sunrise = ds.getSunrise()
-                val nowcast = ds.getNowcast()
-                val locationforecast = ds.getLocationforecast()
-                val fellesDatakatalog = ds.getFellesDatakatalog()
-                _esState.value = ESUiState(sunrise, nowcast, locationforecast, fellesDatakatalog)
+                val sunrise = ds.getSunrise(latitude, longitude, date, offset)
+                val nowcast = ds.getNowcast(latitude, longitude)
+                val locationForecast = ds.getLocationForecast(altitude, latitude, longitude)
+                val openAdress = ds.getOpenAddress(latitude, longitude, radius)
+                _esState.value = ESUiState(sunrise, nowcast, locationForecast, openAdress)
             } catch (e: IOException) {
 
             }
