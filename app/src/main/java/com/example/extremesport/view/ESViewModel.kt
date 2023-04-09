75ac05b0915e2f642831cb835fc59236ca1c3aee
Marius Warlo <mariusiw@uio.no>
2023-04-09 21:18:12 +0200

    Metoden for å sjekke om det anbefales å hoppe er ferdig, men trenger antakeligvis noen små endringer.

--- app/src/main/java/com/example/extremesport/view/ESViewModel.kt
+++ app/src/main/java/com/example/extremesport/view/ESViewModel.kt
@@ -3,10 +3,7 @@ package com.example.extremesport.view
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.extremesport.data.DataSource
-import com.example.extremesport.model.LocationForecastData
-import com.example.extremesport.model.NowcastData
 import com.example.extremesport.model.SportRequirements
-import com.example.extremesport.model.SunriseData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,6 +11,8 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.io.IOException
+import java.util.*
+import kotlin.collections.HashMap
 
 class ESViewModel: ViewModel() {
     // es: ExtremeSport
@@ -34,7 +33,8 @@ class ESViewModel: ViewModel() {
         update(latitude, longitude, altitude, radius, date, offset)
 
         //TODO Midlertidig, dette burde gjøres gjennom datasource og en JSON-fil.
-        sports["Fallskjermhopping"] = SportRequirements(6.26,0.0,0.0,0.0,0.0)
+        //TODO dette er uansett obviously feil. Jeg hadde ikke hoppet i fallskjerm i 9999m/s vind
+        sports["Fallskjermhopping"] = SportRequirements(10000.0,10000.0,10000.0,10000.0,-20.0)
     }
 
     fun update(latitude: Double, longitude: Double, altitude: Int, radius: Int, date: String, offset: String) {
@@ -46,8 +46,6 @@ class ESViewModel: ViewModel() {
                 val locationForecast = ds.getLocationForecast(altitude, latitude, longitude)
                 val openAdress = ds.getOpenAddress(latitude, longitude, radius)
                 _esState.value = ESUiState(sunrise, nowcast, locationForecast, openAdress)
-                //TODO fjern
-                println("Ferdig med å hente api-er")
             } catch (e: IOException) {
 
             }
@@ -55,40 +53,45 @@ class ESViewModel: ViewModel() {
     }
 
     //Returner akkurat nå bare en boolean for om det anbefales å hoppe akkurat nå.
-    fun checkRequirements(sport: String, nowcast: NowcastData, locationForecast: LocationForecastData, sunrise: SunriseData): Boolean {
-        val windspeed: Boolean
-        val precipitation: Boolean
-        val cloud_area_fraction: Boolean
-        val fog_area_fraction: Boolean
-        val temperature: Boolean
-        val sunriseBoolean: Boolean
-        val sunsetBoolean: Boolean
+    fun checkRequirements(sport: String): Boolean {
+        var windspeed = false
+        var precipitation = false
+        var cloud_area_fraction = false
+        var fog_area_fraction = false
+        var temperature = false
+        var sunriseBoolean = false
+        var sunsetBoolean = false
 
         _esState.update { currentState ->
-            println((currentState.nowcast?.type ?: "Feil"))
+            try {
+                val chosenSport = sports[sport]!!
+                val nowcastData = currentState.nowcast?.properties?.timeseries?.get(0)?.data!!
+                val locationForecastData = currentState.locationForecast?.properties?.timeseries?.get(0)?.data!!
+                val sunriseData = currentState.sunrise?.properties!!
+
+                windspeed = nowcastData.instant.details.wind_speed < chosenSport.windspeed
+                precipitation = nowcastData.next_1_hours.details.precipitation_amount < chosenSport.precipitation
+                cloud_area_fraction = locationForecastData.instant.details.cloud_area_fraction < chosenSport.cloud_area_fraction
+                fog_area_fraction = locationForecastData.instant.details.fog_area_fraction < chosenSport.fog_area_fraction
+                temperature = nowcastData.instant.details.air_temperature > chosenSport.temperature
+
+                compareTime(sunriseData.sunrise.time)
+                sunriseBoolean = compareTime(sunriseData.sunrise.time)
+                //sunsetBoolean = !compareTime(sunriseData.sunset.time)
+                sunsetBoolean = true
+            } catch (_: Exception) {
+
+            }
             currentState.copy()
         }
-
-        try {
-            val chosenSport = sports[sport]!!
-            val nowcastData = nowcast.properties.timeseries[0].data
-            val locationForecastData = locationForecast.properties.timeseries[0].data
-            val sunriseData = sunrise.properties
-
-            windspeed = nowcastData.instant.details.wind_speed < chosenSport.windspeed
-            precipitation = nowcastData.next_1_hours.details.precipitation_amount < chosenSport.precipitation
-            cloud_area_fraction = locationForecastData.instant.details.cloud_area_fraction < chosenSport.cloud_area_fraction
-            fog_area_fraction = locationForecastData.instant.details.fog_area_fraction < chosenSport.fog_area_fraction
-            temperature = nowcastData.instant.details.air_temperature > chosenSport.temperature
-            //TODO fjern
-            val currentTime = "Todo"
-            println(currentTime)
-            println("\n\n")
-            sunriseBoolean = true
-            sunsetBoolean = true
-        } catch (e: Exception) {
-            return false
-        }
         return windspeed && precipitation && cloud_area_fraction && fog_area_fraction && temperature && sunriseBoolean && sunsetBoolean
+    }
+
+    //Hjelpemetode for å sammenligne tidspunkter på dagen.
+    fun compareTime(sunriseAPITime: String): Boolean {
+        val rightNow: Calendar = Calendar.getInstance()
+        val realTimeInt = (rightNow.get(Calendar.HOUR_OF_DAY).toString() + rightNow.get(Calendar.MINUTE).toString()).toInt()
+        val sunriseAPIInt = (sunriseAPITime.substring(11,13) + sunriseAPITime.substring(14,16)).toInt()
+        return sunriseAPIInt < realTimeInt
     }
 }
--- app/src/test/java/com/example/extremesport/CheckRequirementsTest.kt
+++ app/src/test/java/com/example/extremesport/CheckRequirementsTest.kt
@@ -1,8 +1,6 @@
 package com.example.extremesport
 
-import com.example.extremesport.data.DataSource
 import com.example.extremesport.view.ESViewModel
-import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Test
 
@@ -10,15 +8,11 @@ class CheckRequirementsTest {
     @Test
     fun parachuteRequirements_isCorrect() {
         val viewmodel = ESViewModel()
-        val dataSource = DataSource()
 
-        Thread.sleep(2000)
+        //TODO venter på at api-ene skal loade, og dette er antakeligvis ikke den beste løsningen lol.
+        Thread.sleep(5000)
 
-        val nowcastTest = runBlocking { dataSource.getNowcast(59.933333, 10.716667) }
-        val locationForecastTest = runBlocking { dataSource.getLocationForecast(1,59.911491,10.757933) }
-        val sunriseTest = runBlocking { dataSource.getSunrise(59.933333,10.716667,"2022-12-18","+01:00") }
-
-        val cRBoolean = viewmodel.checkRequirements("Fallskjermhopping", nowcastTest, locationForecastTest, sunriseTest)
+        val cRBoolean = viewmodel.checkRequirements("Fallskjermhopping")
         Assert.assertEquals(true, cRBoolean)
     }
 }
