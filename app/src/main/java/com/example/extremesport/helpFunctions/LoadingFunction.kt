2696c7db51a3dcceb7b96cf029e18ba60c332e9a
oleelom <oleelom@uio.no>
2023-04-13 15:52:04 +0200

    cleaned up code

--- app/src/main/java/com/example/extremesport/helpFunctions/LoadingFunction.kt
+++ app/src/main/java/com/example/extremesport/helpFunctions/LoadingFunction.kt
@@ -1,23 +1,13 @@
 package com.example.extremesport.helpFunctions
 
-import com.example.extremesport.view.ESUiState
 import com.example.extremesport.view.ESViewModel
 
 fun loadAPIs(vm: ESViewModel) {
 
-    while(vm.esState.value.nowcast == null) {
-        Thread.sleep(1)
-    }
-
-    while(vm.esState.value.sunrise == null) {
-        Thread.sleep(1)
-    }
-
-    while(vm.esState.value.locationForecast == null) {
-        Thread.sleep(1)
-    }
-
-    while(vm.esState.value.openAdress == null) {
+    while(vm.esState.value.nowcast == null
+        || vm.esState.value.sunrise == null
+        || vm.esState.value.locationForecast == null
+        || vm.esState.value.openAdress == null) {
         Thread.sleep(1)
     }
 }
