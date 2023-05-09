8a6e1e52ce20d6896dc14435c3435efa9e0d289b
Marius Warlo <mariusiw@uio.no>
2023-05-09 18:37:28 +0200

    La til test av compareTime

--- app/src/test/java/com/example/extremesport/CheckRequirementsTest.kt
+++ app/src/test/java/com/example/extremesport/CheckRequirementsTest.kt
@@ -1,6 +1,8 @@
 package com.example.extremesport
 
+import com.example.extremesport.data.DataSource
 import com.example.extremesport.view.ESViewModel
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Test
 
@@ -8,11 +10,18 @@ class CheckRequirementsTest {
     @Test
     fun parachuteRequirements_isCorrect() {
         val viewmodel = ESViewModel()
-
-        //TODO venter på at api-ene skal loade, og dette er antakeligvis ikke den beste løsningen lol.
         Thread.sleep(5000)
 
         val cR = viewmodel.checkRequirements("Testing")
         Assert.assertEquals(true, cR.numbAverage > 0)
+    }
+
+    @Test
+    fun compareTimeTest() {
+        val vm = ESViewModel()
+        Thread.sleep(5000)
+
+        Assert.assertEquals(true, vm.compareTime("2022-12-18T00:00+01:00"))
+        //"2022-12-18T09:16+01:00"
     }
 }
